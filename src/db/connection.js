c07625aac5d69f4ecee660866a92536ea48cedde
Rebecca Birkett <rebeccabirkett@outlook.com>
2021-03-25 14:59:20 +0000

    initial setup + mongoose

---
+++ src/db/connection.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const connection = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+        });
+        console.log("successfully connected");
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+connection();
